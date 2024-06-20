fd2b1b1cd35e18731ea1c67bd3ae6660855490ca
hoang19020302 <tranichhoang2001@gmail.com>
2024-06-20 15:47:00 +0700

    Cau hinh Router va Layout cho du an

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// Public routers
+const publicRouters = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// PriUpte routers
+const privateRouters = [];
+
+export { publicRouters, privateRouters };
