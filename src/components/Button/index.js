c5945dbdf7d3ce86470c3c06d075169efa2b8649
hoang19020302 <tranichhoang2001@gmail.com>
2024-06-26 23:44:23 +0700

    header-ui-4

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -56,7 +56,7 @@ function Button({
     });
     return (
         <Comp className={classes} {...props}>
-            {/* {leftIcon && <span className={cx('icon')}>{leftIcon}</span>} */}
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
             <span className={cx('title')}>{children}</span>
             {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
         </Comp>
