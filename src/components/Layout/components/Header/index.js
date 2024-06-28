12b14411c74970d39a3dc56aaa046e90bcb66053
hoang19020302 <tranichhoang2001@gmail.com>
2024-06-28 15:51:27 +0700

    header-ui-5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -26,6 +26,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -46,6 +61,16 @@ function Header() {
             setSearchResult([]);
         }, 0);
     }, []);
+
+    // Handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                // Handle change language
+                break;
+            default:
+        }
+    };
 
     return (
         <header className={cx('wrapper')}>
@@ -86,7 +111,7 @@ function Header() {
                     </Button>
 
                     <Menu
-                        items={MENU_ITEMS}
+                        items={MENU_ITEMS} onChange={handleMenuChange}
                     >
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 
-import styles from './Menu.module.scss';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import Header from './Header';
 import MenuItem from './MenuItem';
+import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
+const defaultFn = () => {};
 
-function Menu({ children, items = [] }) {
+function Menu({ children, items = [], onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
     const renderItems = () => {
-        return items.map((item, index) => <MenuItem key={index} data={item} />);
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+
+            return <MenuItem key={index} data={item} onClick={() => {
+                if (isParent) {
+                    setHistory((prev) => [...prev, item.children]);
+                } else {
+                    onChange(item);
+                }
+            }}/>});
     };
 
     return (
@@ -19,9 +34,15 @@ function Menu({ children, items = [] }) {
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && <Header title='Language' onBack={() => {
+                            setHistory((prev) => prev.slice(0, prev.length - 1));
+                        }} />}
+                        {renderItems()}
+                    </PopperWrapper>
                 </div>
             )}
+            onHide={() => setHistory(prev => prev.slice(0, 1))}
         >
             {children}
         </Tippy>
